c424dac482444ad35d566680196a2558bf7cb96e
Comamoca <amagaeru.dev@gmail.com>
2022-02-23 00:11:41 +0900

    fix and add .so

--- plugin.ts
+++ plugin.ts
@@ -7,18 +7,29 @@ const { filenameBase, pluginBase } = {
     "https://github.com/littledivy/deno_audio/releases/latest/download",
 };
 
-const isDev = Deno.env.get("DEV");
+// const isDev = Deno.env.get("DEV");
 
-if (isDev) {
-  const rid = Deno.dlopen("./target/debug/" + filename(filenameBase));
-} else {
-  // logger.info(`Downloading latest Autopilot release from Github`);
-  const pluginId = await prepare({
-    name: "deno_audio",
-    urls: {
-      darwin: `${pluginBase}/libdeno_audio.dylib`,
-      windows: `${pluginBase}/deno_audio.dll`,
-      linux: `${pluginBase}/libdeno_audio.so`,
-    },
-  });
-}
+// if (isDev) {
+//   const rid = Deno.dlopen("./target/debug/" + filename(filenameBase));
+// } else {
+//   // logger.info(`Downloading latest Autopilot release from Github`);
+//   const pluginId = await prepare({
+//     name: "deno_audio",
+//     urls: {
+//       darwin: `${pluginBase}/libdeno_audio.dylib`,
+//       windows: `${pluginBase}/deno_audio.dll`,
+//       linux: `${pluginBase}/libdeno_audio.so`,
+//     },
+//   });
+// }
+
+
+
+const pluginId = await prepare({
+  name: "deno_audio",
+  urls: {
+    darwin: `${pluginBase}/libdeno_audio.dylib`,
+    windows: `${pluginBase}/deno_audio.dll`,
+    linux: `${pluginBase}/libdeno_audio.so`,
+  },
+});
